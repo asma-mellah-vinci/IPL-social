128ea6a6923c5d60add54b996dac53b46d28fd1a
asma-mellah-vinci <asma.mellah@student.vinci.be>
2026-01-16 16:12:45 +0100

    add: test email with space should be invalid

--- tests/emailValidator.test.ts
+++ tests/emailValidator.test.ts
@@ -9,3 +9,6 @@ test("email sans @ est invalide", () => {
   expect(isValidEmail("testmail.com")).toBe(false);
 });
 
+test("email avec espace est invalide", () => {
+  expect(isValidEmail("test @mail.com")).toBe(false);
+});
