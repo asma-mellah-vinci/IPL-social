90379102f0e9304214616bafe6e0cd4df035bfaa
asma-mellah-vinci <asma.mellah@student.vinci.be>
2026-01-16 16:10:27 +0100

    test: email without @ should be invalid

--- tests/emailValidator.test.ts
+++ tests/emailValidator.test.ts
@@ -4,3 +4,8 @@ import { isValidEmail } from "../src/emailValidator";
 test("email valide", () => {
   expect(isValidEmail("test@mail.com")).toBe(true);
 });
+
+test("email sans @ est invalide", () => {
+  expect(isValidEmail("testmail.com")).toBe(false);
+});
+
